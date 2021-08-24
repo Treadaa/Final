c85a306c73c5b6aa396e4a8b2248f1d917f6d798
ravishrj <87594772+ravishrj@users.noreply.github.com>
2021-08-24 12:18:48 +0530

    Create signinelement.js

---
+++ src/components/signinbox/signinelement.js
@@ -0,0 +1,288 @@
+import styled from "styled-components";
+import {Link as LinkR} from 'react-router-dom';
+// import  {Link} from 'react-router-dom';
+// import Logotreadaa from '../images/whitelogo.png'
+// import importLogotreadaa from '../../images/whitelogo.png'
+ export const Signincontainer=styled.div`
+ background: #0c0c0c;
+display:flex;
+justify-content:center;
+align-items:center;
+padding:0 30px;
+height:656px;
+position:relative;
+z-index:1;
+
+
+`
+export const Signwrap=styled.div`
+
+
+z-index:3;
+max-width:1200px;
+position:relative;
+padding:8px 24px;
+display:flex;
+flex-direction:row;
+height:530px;
+width:522px;
+align-items:center;
+
+`
+ export const Signstartcontainer=styled.div`
+ 
+ 
+position:relative;
+
+display:flex;
+justify-content:center;
+align-items:center;
+height: 89px;
+width: 224px;
+color: wheat;
+bottom: 218px;
+left: 122px;
+flex-direction:row;
+`
+export const Signpagecontainer=styled.div`
+z-index:3;
+max-width:1200px;
+position:relative;
+right:150px;
+padding:8px 24px;
+display:flex;
+flex-direction: row;
+margin-top: 22px;
+height: 354px;
+width: 475px;
+align-items:center;
+background-color: rgb(243, 240, 240);
+border-radius: 12px;
+`
+export const Signpageheaderwrapper=styled.div`
+z-index:3;
+position:relative;
+display:flex;
+flex-direction: row;
+height: 74px;
+width: 422px;
+right: 18px;
+bottom: 139px;
+align-items:center;
+ background-color: rgb(243, 240, 240);
+`
+export const Signpageheaderwrapperh=styled.h1`
+font-size:smaller;
+font-weight:200; 
+`
+export const Signpageheaderlogo=styled.div`
+   z-index:3;
+   position:relative;
+   display:flex;
+   flex-direction: row;
+   height: 23px;
+   width: 22px;
+   left: 29px;
+    top: 7px;
+    align-items:center;   
+    `
+export const Signpagemiddlewrapper=styled.div`
+z-index:3;
+position:absolute;
+ display:flex;
+ flex-direction: row;
+ width: 412px;
+ right: 51px;
+  top: 63px;
+  align-items:center;
+     
+`
+export const Signpagemiddlewrapperemaillabel=styled.div`
+padding-top: 9px;
+    padding-bottom: 8px;
+    
+    color: crimson;
+
+`
+export const Signpagemiddlewrapperform=styled.form`
+height: 148px;
+width: 100%;
+
+
+font-size: larger;
+display: flex;
+padding: 2px;
+margin: 1px;
+flex-direction: column;
+`
+
+export const Input = styled.input`
+border: 1.5px solid red;
+
+/* outline-color: red; */
+outline: none;
+border-radius: 4px;
+width: 420px;
+height: 31px;
+font-size:medium;
+/* margin: 0 0 1em; */
+}
+
+  ::placeholder {
+    color: palevioletred;
+  }
+`
+export const Signpagemiddlewrapperlast=styled.div`
+position:absolute;
+
+    display:flex;
+    flex-direction: row;
+    justify-content:center;
+    align-items:center;
+    height: 98px;
+    width: 447px;
+    top: 162px;
+
+
+
+`
+export const Signpagemiddlewrapperlastbutton=styled.div`
+display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    bottom: 26px;
+    left:76px;
+`
+export const ButtonLink=styled(LinkR)`
+border-radius:18px;
+background:#01bf71;
+white-space:nowwrap;
+padding:10px 22px;
+color:#010606;
+font-size:22px;
+outline:none;
+border:none;
+width: 210px;
+text-align: center;
+cursor:pointer;
+transition:all 0.2s ease-in-out;
+text-decoration :none;
+&:hover{
+    transition:all 0.2s ease-in-out;
+    background:wheat;
+    color:#010606;
+} `
+export const Signpagemiddlewrapperlastfg=styled.div`
+display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    top: 7px;
+    right:96px;
+    margin-top:13px;
+`
+export const FgLink=styled(LinkR)`
+font-size:medium;
+cursor:pointer;
+
+&:hover{
+  transition:all 0.2s ease-in-out;
+  
+  color:wheat;
+}
+`
+export const Signpagemiddlewrapperlastp=styled.p`
+font-size: 8px;
+    display: flex;
+    /* flex-direction: row; */
+    justify-content: center;
+    align-content: space-between;
+    position: absolute;
+    top: 254px;
+    text-align: center;
+
+`
+export const Signlastcontainer=styled.div`
+position:absolute;
+
+    display:flex;
+    flex-direction: row;
+    justify-content:center;
+    align-items:center;
+    height: 73px;
+    width: 455px;
+    bottom: 1px;
+   
+`
+export const Signlastcontainerp=styled.p`
+font-size: 15px;
+color:wheat;
+    display: flex;
+    /* flex-direction: row; */
+    justify-content: center;
+    align-content: space-between;
+    position: relative;
+    bottom:27px;
+    text-align: center;
+    left:164px;
+`
+export const Signlastcontainerbutton=styled.div`
+   display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    right: 47px;
+    top: 11px;
+    
+    
+`
+export const BtnLink=styled(LinkR)`
+border-radius: 11px;
+background: #01bf71;
+
+white-space: nowwrap;
+padding: 10px 22px;
+color: #010606;
+font-size: 16px;
+outline: none;
+border: none;
+width: 304px;
+text-align: center;
+cursor: pointer;
+
+
+text-decoration: none;
+&:hover{
+    transition:all 0.2s ease-in-out;
+    background:wheat;
+    color:black;
+} `
+
+
+
+    
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
