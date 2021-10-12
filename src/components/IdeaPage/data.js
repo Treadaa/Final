18e42c31f17fc0903057db129f35b8721c96862a
Treadaa <87612934+Treadaa@users.noreply.github.com>
2021-10-13 01:16:46 +0530

    Update data.js

--- src/components/IdeaPage/data.js
+++ src/components/IdeaPage/data.js
@@ -1,51 +1,35 @@
 // Gradients taken from: https://webgradients.com/
 const data= [
-    {
-      name: 'Rare Wind',
-      description: '#a8edea → #fed6e3',
-      css: 'linear-gradient(135deg, #a8edea 0%, #fed6e3 100%)',
-      height: 400,
-
-    },
+   
+    
+    
     
     {
-      name: 'Salt Mountain',
-      description: ' #FFFEFF → #D7FFFE',
-      css: 'linear-gradient(135deg, #FFFEFF 0%, #D7FFFE 100%)',
-      height: 400,
-    },
-    {
-      name: 'New York',
-      description: ' #fff1eb → #ace0f9',
-      css: 'linear-gradient(135deg, #fff1eb 0%, #ace0f9 100%)',
-      height: 400,
-    },
-    {
-      name: 'Soft Grass',
+      name: 'Idea A (Content will be written on Web Launch Time)',
       description: ' #c1dfc4 → #deecdd',
       css: 'linear-gradient(135deg, #c1dfc4 0%, #deecdd 100%)',
       height: 400,
     },
     {
-      name: 'Japan Blush',
+      name: 'Idea B (Content will be written on Web Launch Time)',
       description: ' #ddd6f3 → #faaca8',
       css: 'linear-gradient(135deg, #ddd6f3 0%, #faaca8 100%, #faaca8 100%)',
       height: 400,
     },
     {
-        name: 'Japan Blush',
+        name: 'Idea C (Content will be written on Web Launch Time)',
         description: ' #ddd6f3 → #faaca8',
         css: 'linear-gradient(135deg, #ddd6f3 0%, #faaca8 100%, #faaca8 100%)',
         height: 400,
       },
       {
-        name: 'Japan Blush',
+        name: 'Idea D (Content will be written on Web Launch Time)',
         description: ' #ddd6f3 → #faaca8',
         css: 'linear-gradient(135deg, #ddd6f3 0%, #faaca8 100%, #faaca8 100%)',
         height: 400,
       },
       {
-        name: 'Japan Blush',
+        name: 'Idea E (Content will be written on Web Launch Time)',
         description: ' #ddd6f3 → #faaca8',
         css: 'linear-gradient(135deg, #ddd6f3 0%, #faaca8 100%, #faaca8 100%)',
         height: 400,
