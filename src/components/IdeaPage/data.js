b742a61d0e3e2dcc99fe80fe73982efc1fbf8193
Treadaa <87612934+Treadaa@users.noreply.github.com>
2021-10-17 03:43:05 +0530

    Update data.js

--- src/components/IdeaPage/data.js
+++ src/components/IdeaPage/data.js
@@ -9,48 +9,48 @@ const data= [
       height: 400,
     },
    {
-      name: 'Idea A (Content will be written on Web Launch Time)',
+      name: 'Idea B (Content will be written on Web Launch Time)',
       description: ' #c1dfc4 → #deecdd',
       css: 'linear-gradient(135deg, #c1dfc4 0%, #deecdd 100%)',
       height: 400,
     },
    {
-      name: 'Idea A (Content will be written on Web Launch Time)',
+      name: 'Idea C (Content will be written on Web Launch Time)',
       description: ' #c1dfc4 → #deecdd',
       css: 'linear-gradient(135deg, #c1dfc4 0%, #deecdd 100%)',
       height: 400,
     },
     
     {
-      name: 'Idea A (Content will be written on Web Launch Time)',
+      name: 'Idea D (Content will be written on Web Launch Time)',
       description: ' #c1dfc4 → #deecdd',
       css: 'linear-gradient(135deg, #c1dfc4 0%, #deecdd 100%)',
       height: 400,
     },
     {
-      name: 'Idea B (Content will be written on Web Launch Time)',
+      name: 'Idea E (Content will be written on Web Launch Time)',
       description: ' #ddd6f3 → #faaca8',
       css: 'linear-gradient(135deg, #ddd6f3 0%, #faaca8 100%, #faaca8 100%)',
       height: 400,
     },
     {
-        name: 'Idea C (Content will be written on Web Launch Time)',
+        name: 'Idea F (Content will be written on Web Launch Time)',
         description: ' #ddd6f3 → #faaca8',
         css: 'linear-gradient(135deg, #ddd6f3 0%, #faaca8 100%, #faaca8 100%)',
         height: 400,
       },
       {
-        name: 'Idea D (Content will be written on Web Launch Time)',
+        name: 'Idea G (Content will be written on Web Launch Time)',
         description: ' #ddd6f3 → #faaca8',
         css: 'linear-gradient(135deg, #ddd6f3 0%, #faaca8 100%, #faaca8 100%)',
         height: 400,
       },
       {
-        name: 'Idea E (Content will be written on Web Launch Time)',
+        name: 'Idea H (Content will be written on Web Launch Time)',
         description: ' #ddd6f3 → #faaca8',
         css: 'linear-gradient(135deg, #ddd6f3 0%, #faaca8 100%, #faaca8 100%)',
         height: 400,
-        count:"hello i am ravish",
+        
       },
   ]
   export default data;
